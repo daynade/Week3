b71b5479c2377f3386b2dc4b9c1617e1065a08db
DavidAdegboruwa <G00420905@atu.ie>
2025-10-01 16:32:31 +0100

    Created 2 classes, calc class has an add function, mainapp is where the functionsare called

--- src/main/java/ie/atu/MainApp.java
+++ src/main/java/ie/atu/MainApp.java
@@ -1,4 +1,17 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class MainApp {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Please enter the first number");
+        int firstNum = scan.nextInt();
+        System.out.println("Please enter the second number");
+        int secondNum = scan.nextInt();
+
+        Calculator calc =new Calculator();
+        int result = calc.add(2,1);
+    }
 }
