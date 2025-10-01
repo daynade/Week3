606a1b3140dfe2a1c5694e734be235970a99e4f0
DavidAdegboruwa <G00420905@atu.ie>
2025-10-01 16:55:33 +0100

    Added multiplication function

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -9,4 +9,8 @@ public class Calculator {
     {
         return firstNum - secondNum;
     }
+    public int multiply(int firstNum, int secondNum)
+    {
+        return firstNum * secondNum;
+    }
 }
